ecd0437bccaa91cfed78819be5366582b0c5d800
sralloza <sralloza@gmail.com>
2019-09-07 00:23:24 +0200

    Add CLI options from-str and list

--- lens_db/main.py
+++ lens_db/main.py
@@ -1,5 +1,8 @@
 import argparse
+import sys
 
+from .exceptions import BaseLensDBError
+from .utils import exception_exit
 from .core import Lens
 from .scanner import scan
 
@@ -7,14 +10,24 @@ from .scanner import scan
 def get_options(args=None):
     parser = argparse.ArgumentParser('lens-db')
     parser.add_argument('-now', '-today', action='store_true', help='open lens today')
-    parser.add_argument('-days', type=int, help='days after lens were opened')
+    parser.add_argument('-days', type=int, help='days after lens were opened', metavar='days')
     parser.add_argument('-scan', action='store_true', help='scan and send email report if needed')
     parser.add_argument('-last', action='store_true', help='get timestamp of last ')
+    parser.add_argument('-from-str', type=str, help='load date from str', metavar='str')
+    parser.add_argument('-list', action='store_true', help='list database')
 
     return parser.parse_args(args)
 
-
 def main():
+    try:
+        return _main()
+    except BaseLensDBError as exc:
+        exception_exit(exc)
+
+def _main():
+    if len(sys.argv) == 1:
+        sys.argv.append('-h')
+
     options = get_options()
 
     if options.scan:
@@ -26,6 +39,12 @@ def main():
     if options.now:
         return Lens.add(delta_days=0)
 
+    if options.from_str:
+        return Lens.add_custom(options.from_str)
+
+    if options.list:
+        exit(Lens.list())
+
     if options.last:
         last = Lens.get_last()
-        exit('Last lens opened on %r' % last)
+        exit('Last lens opened on %r' % last.strftime('%Y-%m-%d'))
--- lens_db/tests/test_main.py
+++ lens_db/tests/test_main.py
@@ -3,34 +3,52 @@ import pytest
 from lens_db.main import get_options
 
 
-class TestGetOptions:
+def modified_get_options(string: str):
+    return get_options(string.split())
 
+
+class TestGetOptions:
     class TestNow:
         def test_with_argument(self):
             with pytest.raises(SystemExit):
-                get_options('-now 5'.split())
+                modified_get_options('-now 5')
 
         def test_without_argument(self):
-            opt = get_options(['-now'])
+            opt = modified_get_options('-now')
             assert 'now' in opt
             assert opt.now is True
 
     class TestDays:
         def test_with_number(self):
-            opt = get_options('-days 5'.split())
+            opt = modified_get_options('-days 5')
             assert 'days' in opt
             assert opt.days == 5
 
         def test_without_number(self):
             with pytest.raises(SystemExit):
-                get_options('-days'.split())
+                modified_get_options('-days')
 
     def test_scan(self):
-        opt = get_options('-scan'.split())
+        opt = modified_get_options('-scan')
         assert 'scan' in opt
         assert opt.scan is True
 
     def test_last(self):
-        opt = get_options('-last'.split())
+        opt = modified_get_options('-last')
         assert 'last' in opt
         assert opt.last is True
+
+    class TestFromStr:
+        def test_with_argument(self):
+            opt = get_options(['-from-str', 'some old string'])
+            assert 'from_str' in opt
+            assert opt.from_str == 'some old string'
+
+        def test_without_argument(self):
+            with pytest.raises(SystemExit):
+                modified_get_options('-from-str')
+
+    def test_list(self):
+        opt = modified_get_options('-list')
+        assert 'list' in opt
+        assert opt.list is True
