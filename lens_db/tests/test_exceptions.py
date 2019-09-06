557beff3237011476122a5994153a57ecc2eb138
sralloza <sralloza@gmail.com>
2019-09-06 13:45:03 +0200

    Add tests for exceptions

---
+++ lens_db/tests/test_exceptions.py
@@ -0,0 +1,24 @@
+import pytest
+
+from lens_db.exceptions import BaseLensDBError, AlreadyAddedError, InvalidDateError
+
+
+def test_base_lens_db_error():
+    with pytest.raises(BaseLensDBError):
+        raise BaseLensDBError
+
+    assert issubclass(BaseLensDBError, Exception)
+
+
+def test_already_added_error():
+    with pytest.raises(AlreadyAddedError):
+        raise AlreadyAddedError
+
+    assert issubclass(AlreadyAddedError, BaseLensDBError)
+
+
+def test_invalid_date_error():
+    with pytest.raises(InvalidDateError):
+        raise InvalidDateError
+
+    assert issubclass(InvalidDateError, BaseLensDBError)
