b46e29424ccebb83b4f15f0e5b23cb10c28847d6
sralloza <sralloza@gmail.com>
2019-09-06 12:47:04 +0200

    Fix scanner

--- lens_db/scanner.py
+++ lens_db/scanner.py
@@ -1,5 +1,5 @@
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from allo_mail import send_email
 
@@ -15,14 +15,29 @@ def scan():
         logger.debug('No entries')
         return
 
-    today = datetime.today().date()
+    today = today_date()
     delta = today - last
     logger.debug('Calculated delta of %s days', delta.days)
 
     if delta > LENS_DURABILITY_DELTA:
-        logger.debug('Delta > %s days, sending email', LENS_DURABILITY_DELTA.days)
+        logger.debug('Delta > %s days, sending email (expired)', LENS_DURABILITY_DELTA.days)
         message = 'Hay que cambiar las lentillas, el último cambio fue el %s (%s días)' % (
             last, delta)
-        return send_email(ADMIN_EMAIL, 'Cambiar lentillas', message, name='Lens-db')
+        return send_email(ADMIN_EMAIL, 'Cambiar lentillas YA', message, name='Lens-db')
+    if delta == LENS_DURABILITY_DELTA:
+        logger.debug('Delta > %s days, sending email (tomorrow)', LENS_DURABILITY_DELTA.days)
+        message = 'Hay que cambiar las lentillas, el último cambio fue el %s (%s días)' % (
+            last, delta)
+        return send_email(ADMIN_EMAIL, 'Cambiar lentillas mañana', message, name='Lens-db')
+    elif delta == LENS_DURABILITY_DELTA - timedelta(days=1):
+        logger.debug('Delta > %s days, sending email (day after tomorrow)', LENS_DURABILITY_DELTA.days)
+        message = 'Mañana hay que cambiar las lentillas, el último cambio fue el %s (%s días)' % (
+            last, delta)
+        return send_email(ADMIN_EMAIL, 'Cambiar lentillas pasado mañana', message, name='Lens-db')
 
     logger.debug('%d days left with current lens', LENS_DURABILITY_DELTA.days - delta.days)
+
+
+def today_date():
+    # TODO: move to other file
+    return datetime.today().date()
