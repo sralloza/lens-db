a8e80b29ef303fd4efbe9d15972192335b14cc2d
sralloza <sralloza@gmail.com>
2019-09-07 00:22:58 +0200

    Add function exception_exit()

--- lens_db/tests/test_utils.py
+++ lens_db/tests/test_utils.py
@@ -1,7 +1,31 @@
 from datetime import datetime
 
-from lens_db.utils import today_date
+import pytest
+
+from lens_db.utils import today_date, exception_exit
 
 
 def test_today_date():
     assert today_date() == datetime.today().date()
+
+
+exceptions = (
+    (ValueError, 'Invalid path'),
+    (TypeError, ('Invalid type', 'Expected int')),
+    (ImportError, 'Module not found: math')
+)
+
+
+@pytest.mark.parametrize('exception, args', exceptions)
+def test_exception_exit(exception, args):
+    if not isinstance(args, str):
+        message = ', '.join(args)
+    else:
+        message = args
+        args = (args, )
+
+    with pytest.raises(SystemExit, match=message):
+        try:
+            raise exception(*args)
+        except Exception as err:
+            exception_exit(err)
--- lens_db/utils.py
+++ lens_db/utils.py
@@ -3,3 +3,7 @@ from datetime import datetime
 
 def today_date():
     return datetime.today().date()
+
+
+def exception_exit(exception):
+    exit('%s: %s' % (exception.__class__.__name__, ', '.join(exception.args)))
