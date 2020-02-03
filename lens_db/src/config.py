f4ebca638ab9416e0aed9ee8d63f7b880751745c
sralloza <sralloza@gmail.com>
2020-02-03 11:52:43 +0100

    Fix LOGGING_PATH, DATABASE_PATH and DISABLED path

--- lens_db/src/config.py
+++ lens_db/src/config.py
@@ -14,6 +14,6 @@ LENS_DURABILITY = 15  # In days
 LENS_DURABILITY_DELTA = timedelta(days=LENS_DURABILITY)
 ADMIN_EMAIL = "sralloza@gmail.com"
 
-LOGGING_PATH = Path(__file__).parent.parent / "lens-db.log"
-DATABASE_PATH = Path(__file__).parent.parent / "lens.db"
-DISABLED = Path(__file__).parent.parent.joinpath("disabled").exists()
+LOGGING_PATH = Path(__file__).parent.parent.parent / "lens-db.log"
+DATABASE_PATH = Path(__file__).parent.parent.parent / "lens.db"
+DISABLED = Path(__file__).parent.parent.parent.joinpath("disabled").exists()
